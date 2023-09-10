bbb5e90ed41e5c33e23d5b7169a395bcc1b45747
Ben Percy <ben.percy@silverstripe.com>
2023-09-11 11:54:11 +1200

    Cleaned up indentation and added gitignore

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Install, configure and register Tenable's Nessus agent with service
 
 This will download and install the Nessus agent from specified URL.
 
-Configuration is done via the `/opt/nessus_agent/sbin/nessuscli` CLI application installed when the agent package.
+Configuration is done via the `/opt/nessus_agent/sbin/nessuscli` CLI application installed, when the agent package has been installed.
 
 This will link the agent by running the commands:
 
@@ -20,3 +20,17 @@ Once registered and enabled the secret (passphrase) is required to reconfigure a
 This will link the agent with the Tenable Cloud, and can be configured through the UI to have scans run on it. 
 
 Once the agent is installed, setup and enabled, you can link/start and unlink/stop the agent via puppet, by setting the `nessus_agent_enabled` parameter to true/false.
+
+## Parameters
+
+`http_proxy`: a string detailing the proxy host and port ie 'blah.co.nz:111' which if defined, will be added to the curl request to download the nessus agent package
+
+`http_proxy_port`: the port used by the proxy
+
+`http_proxy_host`: the host used by the proxy
+
+`link_token`: A parameter which defines the key/token needed to link the nessus agent to tenable cloud
+
+`download_url`: The URL to download the nessus agent package from
+
+`nessus_agent_enabled` A parameter which if set (to true), will install, start, and link the agent to tenable cloud. If not set (or set to anything other than true), it will unlink/stop the agent (if it is currently running/linked)
--- manifests/agent.pp
+++ manifests/agent.pp
@@ -22,29 +22,29 @@ class ss_nessus_agent::agent inherits ::ss_nessus_agent {
   if $params::nessus_agent_enabled == true {
     if !$facts['ss_nessus_agent_connected'] {
       exec { 'ss_nessus_agent_install_agent':
-      command => "sudo -u root dpkg -i /usr/src/nessusagent.deb",
-      path    => '/usr/bin:/usr/sbin:/bin',
+        command => "sudo -u root dpkg -i /usr/src/nessusagent.deb",
+        path    => '/usr/bin:/usr/sbin:/bin',
       }
       -> exec { 'ss_nessus_agent_start_agent':
-      command => "sudo -u root systemctl start nessusagent",
-      path    => '/usr/bin:/usr/sbin:/bin',
+        command => "sudo -u root systemctl start nessusagent",
+        path    => '/usr/bin:/usr/sbin:/bin',
       }
       -> exec { 'ss_nessus_agent_link_agent':
-      command => "/opt/nessus_agent/sbin/nessuscli agent link --key=${ss_nessus_agent::link_token} name=${$facts['environmentname']} --cloud --proxy-host=${ss_nessus_agent::http_proxy_host} --proxy-port=${ss_nessus_agent::http_proxy_port}",
-      path    => '/usr/bin:/usr/sbin:/bin',
+        command => "/opt/nessus_agent/sbin/nessuscli agent link --key=${ss_nessus_agent::link_token} name=${$facts['environmentname']} --cloud --proxy-host=${ss_nessus_agent::http_proxy_host} --proxy-port=${ss_nessus_agent::http_proxy_port}",
+        path    => '/usr/bin:/usr/sbin:/bin',
       }
     }
   } else {
     if $facts['ss_nessus_agent_connected'] {
       exec { 'ss_nessus_agent_unlink_agent':
-      command => "/opt/nessus_agent/sbin/nessuscli agent unlink",
-      path    => '/usr/bin:/usr/sbin:/bin',
+        command => "/opt/nessus_agent/sbin/nessuscli agent unlink",
+        path    => '/usr/bin:/usr/sbin:/bin',
       }
     }
     if $facts['ss_nessus_agent_running'] {
       exec { 'ss_nessus_agent_stop_agent':
-      command => "sudo -u root systemctl stop nessusagent",
-      path    => '/usr/bin:/usr/sbin:/bin',
+        command => "sudo -u root systemctl stop nessusagent",
+        path    => '/usr/bin:/usr/sbin:/bin',
       }
     }
   }
